c560c5487f92437d3cb218476a75697cf0c57388
SatyaJaiss <satyamjais4981@gmail.com>
2024-10-06 02:35:27 +0530

    Updated Responsiveness
    I have updated the responsiveness of the  page, now they are responsive to all screen sizes. Also added a hamburger button to show the menu

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 
 function Navbar() {
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsActive(!isActive);
+  };
+
+  const closeNavbar = () => {
+    setIsActive(false);
+  };
+
   return (
-    <nav className="navbar">
+    <nav className={`navbar`}>
       <div className="container">
         <img src="/images/e1.png" alt="Logo" className="logo" />
+        <button className="navbar-toggle" onClick={toggleNavbar}>
+          ☰ {/* Hamburger icon */}
+        </button>
+      </div>
+      <div className={`nav-menu ${isActive ? 'active' : ''}`}>
+        <button className="close-button" onClick={closeNavbar}>
+          &times; {/* Close icon */}
+        </button>
         <ul className="nav-links">
-          <li><a href="/">Home</a></li>
-          <li><a href="/about">About Us</a></li>
-          <li><a href="/contact">Contact Us</a></li>
+          <li><a href="/" onClick={closeNavbar}>Home</a></li>
+          <li><a href="/about" onClick={closeNavbar}>About Us</a></li>
+          <li><a href="/contact" onClick={closeNavbar}>Contact Us</a></li>
         </ul>
       </div>
     </nav>

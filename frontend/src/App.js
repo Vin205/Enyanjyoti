518ef21dd8382d4513d95380202b8e59de2f745d
ARJUN AGARWAL <123585663+sahhoArjun097@users.noreply.github.com>
2024-10-06 22:26:26 +0530

    added about us page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom"; 
 import { useLocation } from "react-router-dom"; // Resolved spacing issue
 
-import { Routes, Route, useLocation } from "react-router-dom"; // Reso0b14a140d
+// import { Routes, Route, useLocation } from "react-router-dom"; // Reso0b14a140d
 
 import Navbar from "./components/Navbar/Navbar.js";
 import Hom from "./pages/Home/Hom.js";

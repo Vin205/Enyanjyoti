af771aa92f513ddf04b8a6d0aae91d48dc473f56
Priyanshi-codes <gupta28priyanshi@gmail.com>
2024-10-06 18:05:18 +0530

    added a profile card

---
+++ backend/src/Server.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ProfileCard from '../Component/profileCard/ProfileCard';
+
+const StudentDashboard = () => {
+  return (
+    <div>
+      <h2>Student Dashboard</h2>
+      <ProfileCard />
+      
+    </div>
+  );
+};
+
+export default StudentDashboard;

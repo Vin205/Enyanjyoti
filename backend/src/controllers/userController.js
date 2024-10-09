7abe5e4b15da94eda3ed23f2cac1b56e16d7a873
tejasbenibagde <124677750+tejasbenibagde@users.noreply.github.com>
2024-10-09 22:30:11 +0530

    Add authentication routes for user registration and login

--- backend/src/controllers/userController.js
+++
@@ -1,8 +0,0 @@
-// controllers/userController.js
-export const getUsers = (req, res) => {
-    res.json({ message: "Get Users" });
-};
-
-export const createUser = (req, res) => {
-    res.json({ message: "User Created" });
-};

2e47e265b4eb3e585b03bf78c5e0c2bd13855bfc
tejasbenibagde <124677750+tejasbenibagde@users.noreply.github.com>
2024-10-11 23:19:10 +0530

    Setup Test 3

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,6 @@ app.use(express.json());
 
 // Use the user routes
 app.use('/api/auth', authRoutes);
-
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);

3ea40f2e2afd25f12b995eceeccd4dd7a9dd7154
Raj Kumar Yadav <rajkumar08bgh@gmail.com>
2024-10-20 16:16:23 +0530

    Add dark mode

--- backend/src/controllers/index.js
+++ backend/src/controllers/index.js
@@ -1,4 +1,3 @@
-// auth
-
-export { registerUser } from './auth/registerUser.js';
-export { loginUser } from './auth/loginUser.js';
+version https://git-lfs.github.com/spec/v1
+oid sha256:08af3166eb54288db131ec557f2de729ba4a1d62ab00e86a22790022be45fa10
+size 113
--- backend/src/utils/db/connectDB.js
+++ backend/src/utils/db/connectDB.js
@@ -1,32 +1,3 @@
-// db.js
-import mongoose from 'mongoose';
-
-/**
- * Connect to MongoDB
- * @param {string} uri - MongoDB connection URI
- */
-const connectDB = async (uri) => {
-  // Check if the URI is provided
-  if (!uri) {
-    console.error('MongoDB URI is not provided.');
-    process.exit(1); // Exit the process with failure
-  }
-
-  // Validate the URI format (basic validation)
-  const uriPattern =
-    /^(mongodb(?:\+srv)?:\/\/)([a-zA-Z0-9._-]+:[a-zA-Z0-9._-]+@)?([a-zA-Z0-9.-]+)(:[0-9]{1,5})?(\/[a-zA-Z0-9._-]*)?$/;
-  if (!uriPattern.test(uri)) {
-    console.error('Invalid MongoDB URI format:', uri);
-    process.exit(1); // Exit the process with failure
-  }
-
-  try {
-    await mongoose.connect(uri);
-    console.log('MongoDB connected successfully');
-  } catch (error) {
-    console.error('MongoDB connection error:', error.message);
-    process.exit(1);
-  }
-};
-
-export default connectDB;
+version https://git-lfs.github.com/spec/v1
+oid sha256:6bb994e70408eab28b1e0bd51e57641eb9d968da24a6bd96cae11a9fb25f0949
+size 877

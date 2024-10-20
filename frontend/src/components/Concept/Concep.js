753d2f318280f6436f293fec584df815ee95daaa
Tina <tinaborundia23@gmail.com>
2024-10-20 16:38:49 +0530

    Multi-Language Support Using Language Translator

--- frontend/src/components/Concept/Concep.js
+++ frontend/src/components/Concept/Concep.js
@@ -1,23 +1,3 @@
-import React from 'react'
-function Concep  ()  {
-  return (
-    <>
-    <div class="py-5">
-     <h1 align="center">Conceptual Learning</h1>
-    </div>
-
-<div align="center" class="py-5">
- 
-<iframe width="560" height="315" src="https://www.youtube.com/embed/tBzRK7CIPpk?si=qEOPPZAazZdAQdk2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-<h3>Hindi Rhymes for Kids</h3>
-</div>
-<div align ="center" class="py-5">
-<iframe width="560" height="315" src="https://www.youtube.com/embed/qRNeIBWvE9o?si=i-AiAPi2GIHjiQoq" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-<h3>Mathematics basic Maths</h3>
-</div>
-
-
-    </>
-  )
-}
-export default Concep
+version https://git-lfs.github.com/spec/v1
+oid sha256:88329d66dd7d7974a7d93c08013b729e867ebf864e4e9b313c6832605eed493f
+size 897
